93e0cb618849289252e34f95e447e694d65c519c
Gabriel Haruki <gabrielharukisatoh@gmail.com>
2021-02-05 17:54:10 -0300

    feat(web-api): implementing base controllers/models

---
+++ WebAPI/WebApplication1/Controllers/ClientController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClientController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = "SELECT c.*, MAX(e.EvaluationReferenceDate) as LastEvaluationReferenceDate " +
+                "FROM Evaluation as e " +
+                "INNER JOIN ClientEvaluation as ce " +
+                    "ON e.EvaluationId = ce.EvaluationId " +
+                "INNER JOIN Client as c " +
+                    "ON c.ClientId = ce.ClientId " +
+                    "GROUP BY ce.ClientId, c.ClientId, c.ClientCompanyName, c.ClientContactName, " +
+                            "c.ClientCNPJ, c.JoiningDate, c.LastEvaluationCategory";
+
+            DataTable table = new DataTable();
+            using(var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using(var cmd = new SqlCommand(query, con))
+            using(var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        public HttpResponseMessage Post(Client client)
+        {
+            try
+            {
+                string query = $"INSERT INTO dbo.Client" +
+                    $"(ClientCompanyName, ClientContactName, ClientCNPJ) VALUES (" +
+                    $"'{client.ClientCompanyName}'," +
+                    $"'{client.ClientContactName}'," +
+                    $"'{client.ClientCNPJ}')";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { message = "Client created succesfully!" });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { error = e.Message });
+            }
+        }
+
+        public HttpResponseMessage Put(Client client)
+        {
+            try
+            {
+                string query = $"UPDATE dbo.Client SET " +
+                        $"ClientCompanyName='{client.ClientCompanyName}'," +
+                        $"ClientContactName='{client.ClientContactName}'," +
+                        $"ClientCNPJ='{client.ClientCNPJ}'" +
+                    $"WHERE ClientId={client.ClientId}";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { message = "Client updated succesfully!" });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { error = e.Message });
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                string query = $"DELETE FROM dbo.Client WHERE ClientId={id}";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { message = "Client deleted succesfully!" });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { error = e.Message });
+            }
+        }
+
+        public class SearchRequest
+        {
+            public string ClientCompanyName { get; set; }
+        }
+
+        [Route("api/Client/search")]
+        [HttpGet]
+        public HttpResponseMessage GetClientsFromName([FromBody] SearchRequest searchRequest)
+        {
+            try
+            {
+                string query = "SELECT c.*, MAX(e.EvaluationReferenceDate) as LastEvaluationReferenceDate " +
+                    "FROM Evaluation as e " +
+                    "INNER JOIN ClientEvaluation as ce " +
+                        "ON e.EvaluationId = ce.EvaluationId " +
+                    "INNER JOIN Client as c " +
+                        "ON c.ClientId = ce.ClientId " +
+                        $"WHERE c.ClientCompanyName = '{searchRequest.ClientCompanyName}' " +
+                        "GROUP BY ce.ClientId, c.ClientId, c.ClientCompanyName, c.ClientContactName, " +
+                                "c.ClientCNPJ, c.JoiningDate, c.LastEvaluationCategory";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, table);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { error = e.Message });
+            }
+            
+        }
+    }
+}
---
+++ WebAPI/WebApplication1/Controllers/ClientEvaluationController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClientEvaluationController : ApiController
+    {
+        [Route("api/ClientEvaluation/all")]
+        public HttpResponseMessage Get()
+        {
+            string query = "SELECT * FROM dbo.ClientEvaluation";
+
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+        }
+
+        [Route("api/Client/{clientId}/Evaluation/{evaluationId}")]
+        public HttpResponseMessage Post(ClientEvaluation clientEvaluation, int clientId, int evaluationId)
+        {
+            try
+            {
+                string query = $"INSERT INTO dbo.ClientEvaluation " +
+                    $"VALUES({clientId}, " +
+                        $"{evaluationId}, " +
+                        $"{clientEvaluation.Grade}, " +
+                        $"'{clientEvaluation.Reason}')";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { message = "Client evaluation created succesfully!" });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { error = e.ToString() });
+            }
+        }
+
+        [Route("api/Client/{clientId}/Evaluation/{evaluationId}")]
+        public HttpResponseMessage Put(ClientEvaluation clientEvaluation, int clientId, int evaluationId)
+        {
+            try
+            {
+                string query = $"UPDATE dbo.ClientEvaluation SET " +
+                        $"Grade='{clientEvaluation.Grade}', " +
+                        $"Reason='{clientEvaluation.Reason}' " +
+                    $"WHERE ClientId={clientId} AND EvaluationId={evaluationId}";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { message = "Client evaluation updated succesfully!" });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { error = e.Message });
+            }
+        }
+
+        [Route("api/Client/{clientId}/Evaluation/{evaluationId}")]
+        public HttpResponseMessage Delete(int clientId, int evaluationId)
+        {
+            try
+            {
+                string query = $"DELETE FROM dbo.ClientEvaluation WHERE ClientId={clientId} AND EvaluationId={evaluationId}";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["DesafioForLogicAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new { message = "Client evaluation deleted succesfully!" });
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, new { error = e.Message });
+            }
+        }
+    }
+}
